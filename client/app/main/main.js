f90b5b4e1e30c6bdbd1a5b0a79a2beed58475b9f
terrence <parliamentaryterrence@gmail.com>
2015-02-28 12:37:07 -0500

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('relationalDataApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl',
+        pageTitle: "Main"
+      });
+  });
